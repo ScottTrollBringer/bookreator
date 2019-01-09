fefcaa4af9a1921d2ecd1d47a650df9f6efd9917
Ian Fraser <ian.fraser@decathlon.com>
2019-01-09 13:58:49 +0100

    Initialization

---
+++ src/routes.ts
@@ -0,0 +1,12 @@
+import express = require('express');
+
+// Express setup
+const app = express();
+app.use(express.static(__dirname + '/../views/'));
+app.listen(5000);
+
+// Default route - SPA home page
+const path = require('path');
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../views/index.html'));
+});
