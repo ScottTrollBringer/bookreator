257c05cd94fd921cc52f49c5bb9caf13230d3b3e
Ian Fraser <ian.fraser@decathlon.com>
2019-07-18 19:02:21 +0200

    Create admin page UI

--- src/Admin.elm
+++ src/Admin.elm
@@ -1,16 +1,19 @@
 module Admin exposing (..)
 
 import Browser
-import Html exposing (Html, text, h1, table, tr, td)
+import Html exposing (Html, table, text, tr, td, button, textarea, input, h1)
+import Html.Attributes exposing (cols, rows, align, id, placeholder, size)
 
 
 main =
     Browser.element { init = init, update = update, subscriptions = subscriptions, view = view }
 
 
-type Msg = Increment
+type Msg
+    = Increment
 
-type Model = Placeholder
+type Model
+    = Placeholder
 
 
 init : () -> ( Model, Cmd Msg )
@@ -27,11 +30,26 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  table [] [
-    tr [] [
-      td [] [ h1 [] [ text "MyBalls" ] ]
-    ]
-  ]
+    table [ align "center" ]
+        [ tr []
+          [ td [] [ h1 [ id "bookTitle" ] [ text "Create a page" ] ]
+          ]
+        , tr []
+          [ td [] [ textarea [ cols 80, rows 30, id "pageContent" ] [] ]
+          ]
+        , tr []
+          [ td [] [ input [ placeholder "Optional 1st choice", id "choiceOne", size 80 ] [] ]
+          ]
+        , tr []
+          [ td [] [ input [ placeholder "Optional 2nd choice", id "choiceTwo", size 80 ] [] ]
+          ]
+        , tr []
+          [ td [] [ input [ placeholder "Page number", id "pageNumber" ] [] ]
+          ]
+        , tr []
+          [ td [] [ button [] [ text "Create page" ] ]
+          ]
+        ]
 
 
 subscriptions : Model -> Sub Msg
